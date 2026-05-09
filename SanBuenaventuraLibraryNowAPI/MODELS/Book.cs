9cb6b8d5168152c3675faeb57969d0a639a408bb
ughlann <allanfancissanbuenaventura1519@gmail.com>
2026-05-09 12:54:13 +0800

    Update Book.cs

--- SanBuenaventuraLibraryNowAPI/MODELS/Book.cs
+++ SanBuenaventuraLibraryNowAPI/MODELS/Book.cs
@@ -1,14 +1,12 @@
-﻿namespace SanBuenaventuraLibraryNowAPI.MODELS
+namespace SanBuenaventuraLibraryNowAPI.Models
 {
-    public class book
+    public class Book
     {
-    public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Author { get; set; }
-        public string? Genre { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
         public bool Available { get; set; }
         public int PublishedYear { get; set; }
-
-
     }
 }
